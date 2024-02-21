373c664ec618c7ba03530f92d87674c8acaf779d
adegbolaa <akotraining1@gmail.com>
2024-02-21 13:01:18 -0600

    adding shared-lib

---
+++ vars/buildDockerImage.groovy
@@ -0,0 +1,10 @@
+def call() {
+    echo 'Building docker image'
+    withCredentials([
+        usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')])
+     {
+            sh 'docker build -t kevinlearningaccount/java-maven:2.0 .'
+            sh 'echo $PASS docker login -u $USER --password-stdin'
+            sh 'docker push kevinlearningaccount/java-maven:2.0'
+    }
+}
---
+++ vars/buildJar.groovy
@@ -0,0 +1,4 @@
+   def call(){
+        echo "Building"
+        sh 'mvn package'
+    }
