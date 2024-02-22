6ed71ed4734d2ce9bf8a2d22ca6556f8fc24a5ea
adegbolaa <akotraining1@gmail.com>
2024-02-22 11:22:47 -0600

    refactor

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,13 +1,13 @@
 #!/user/bin/env groovy
 
 def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
-// echo 'Building docker image'
-// withCredentials([
-//     usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')])
-//  {
-//         sh 'docker build -t kevinlearningaccount/java-maven:2.0 .'
-//         sh 'echo $PASS docker login -u $USER --password-stdin'
-//         sh 'docker push kevinlearningaccount/java-maven:2.0'
-// }
+   // return new Docker(this).buildDockerImage(imageName)
+    echo 'Building docker image'
+    withCredentials([
+        usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')])
+    {
+            sh "docker build -t $imageName ."
+            sh 'echo $PASS docker login -u $USER --password-stdin'
+            sh "docker push $imageName"
+    }
 }
